c12811bac131c6ade81bc918cb941843076169e9
Daniel Pereira <pereiradpg@yahoo.ca>
2017-06-09 13:14:53 -0400

    Notation added to about_strings

--- about_strings.rb
+++ about_strings.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutStrings < Neo::Koan
   def test_double_quoted_strings_are_strings
     string = "Hello, World"
+    # Check the class of string with .is_a?
+    #   .is_a?(class) -> true or false
     assert_equal true, string.is_a?(String)
   end
 
@@ -13,6 +15,7 @@ class AboutStrings < Neo::Koan
 
   def test_use_single_quotes_to_create_string_with_double_quotes
     string = 'He said, "Go Away."'
+    # assert_equal compares the two arguments and returns true or false
     assert_equal "He said, \"Go Away.\"", string
   end
 
@@ -40,18 +43,53 @@ class AboutStrings < Neo::Koan
 It was the best of times,
 It was the worst of times.
 }
+    # String.length returns an integer representing the length of String
     assert_equal 54, long_string.length
+    # String.lines(separator=$/) -> an_array
+    #   Returns an array of lines in str split using the supplied record separator ($/ by default).  This s a shorthand for str.each_line(separator).to_a.
+    # Array.count(obj) -> int
+    #   Returns the number of elements
     assert_equal 3, long_string.lines.count
     assert_equal "\n", long_string[0,1]
   end
 
   def test_here_documents_can_also_handle_multiple_lines
+    # The following is called a "here document".
+    #   Construct a here document using the << operator followed by an identifier that marks the end of the here document.  The end mark is called the terminator.
+
+    # puts <<GROCERY_LIST
+    # Grocery list
+    # ------------
+    # 1. Salad mix.
+    # 2. Strawberries.*
+    # 3. Cereal.
+    # 4. Milk.*
+
+    # * Organic
+    # GROCERY_LIST
+    # The result:
+
+    # $ grocery-list.rb
+    # Grocery list
+    # ------------
+    # 1. Salad mix.
+    # 2. Strawberries.*
+    # 3. Cereal.
+    # 4. Milk.*
+
+    # * Organic
+
+    # Read more here: https://en.wikibooks.org/wiki/Ruby_Programming/Here_documents
+
     long_string = <<EOS
 It was the best of times,
 It was the worst of times.
 EOS
+    
     assert_equal 53, long_string.length
     assert_equal 2, long_string.lines.count
+    # String#[]
+    #   str[start, length]
     assert_equal "I", long_string[0,1]
   end
 
@@ -88,6 +126,9 @@ EOS
   def test_the_shovel_operator_will_also_append_content_to_a_string
     hi = "Hello, "
     there = "World"
+    # String#<<
+    #   str << obj ->
+    #   Append-Concatenates the given object to str.
     hi << there
     assert_equal "Hello, World", hi
     assert_equal "World", there
@@ -108,6 +149,9 @@ EOS
 
   def test_double_quoted_string_interpret_escape_characters
     string = "\n"
+    # String#size
+    #   size -> integer
+    #   Returns the character length of str
     assert_equal 1, string.size
   end
 
@@ -135,6 +179,8 @@ EOS
   end
 
   def test_any_ruby_expression_may_be_interpolated
+    # Math.sqrt(a)
+    #   Computes the square root of a.
     string = "The square root of 5 is #{Math.sqrt(5)}"
     assert_equal "The square root of 5 is 2.23606797749979", string
   end
@@ -176,6 +222,8 @@ EOS
 
   def test_strings_can_be_split
     string = "Sausage Egg Cheese"
+    # String#split(pattern=nil, [limit]) -> anArray
+    #   Divides str into substrings based on a delimiter, returning an array of these substrings.  If pattern is a string then it's contents are used as the delimiter when splitting str.
     words = string.split
     assert_equal ["Sausage", "Egg", "Cheese"], words
   end
@@ -192,6 +240,8 @@ EOS
 
   def test_strings_can_be_joined
     words = ["Now", "is", "the", "time"]
+    # Array#join(sepearator=$,) -> str
+    #   Returns a string created by converting each element of the array to a string, separated by the given separator.
     assert_equal "Now is the time", words.join(" ")
   end
 
@@ -199,6 +249,8 @@ EOS
     a = "a string"
     b = "a string"
 
+    # Object#object_id -> integer
+    #   Returns an integer identifier for obj.
     assert_equal true, a           == b
     assert_equal false, a.object_id == b.object_id
   end
